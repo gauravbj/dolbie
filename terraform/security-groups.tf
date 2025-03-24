c4c113f5eb80b4fc171917ca5c8d97bc293ae5eb
gauravbj <gauravbjain.25@gmail.com>
2025-03-24 14:29:40 -0700

    Update security-groups.tf

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,5 +1,5 @@
 
-resource "aws_security_group" "worker_group_mgmt_one" {
+     resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   vpc_id      = module.vpc.vpc_id
 
